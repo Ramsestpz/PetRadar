e6b2a52fd642ce6bb71ebc385cae9211968745e4
Ramsestpz <tepozramses@gmail.com>
2026-06-21 01:29:10 -0600

    Preparando API para producción sin Redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,30 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config'; 
-import { CacheModule } from '@nestjs/cache-manager';
-import * as redisStore from 'cache-manager-redis-store';
+// import { CacheModule } from '@nestjs/cache-manager';
+// import * as redisStore from 'cache-manager-redis-store';
 import { PetsModule } from './pets/pets.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }), 
-    CacheModule.register({
-      isGlobal: true,
-      store: redisStore as any,
-      host: process.env.REDIS_HOST || 'localhost',
-      port: parseInt(process.env.REDIS_PORT || '6379'),
-      ttl: 300, // 5 minutes
-    }),
+    
+    // CacheModule.register({
+    //   isGlobal: true,
+    //   store: redisStore as any,
+    //   host: process.env.REDIS_HOST || 'localhost',
+    //   port: parseInt(process.env.REDIS_PORT || '6379'),
+    //   ttl: 300, 
+    // }),
+
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT || '5432'),
-      username: process.env.DB_USERNAME || 'postgres',
-      password: process.env.DB_PASSWORD || 'root', 
-      database: process.env.DB_NAME || 'petradar',
+      url: process.env.DATABASE_URL || `postgresql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
       autoLoadEntities: true,
       synchronize: true, 
+      ssl: process.env.DB_HOST !== 'localhost' ? {
+        rejectUnauthorized: false,
+      } : false,
     }),
     PetsModule,
   ],
